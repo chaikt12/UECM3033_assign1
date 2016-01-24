ed8d337c6dc3654eab7e8611a6da91bff9a0125b
Chai Kun Ting <chaikt12@1utar.edu.my>
2016-01-24 22:52:08 +0800

    added python scrpit bt nt finish yet

--- pyexercise.py
+++ pyexercise.py
@@ -1,5 +1,7 @@
 import sympy as sy
-import numpy as np
+import numpy as np 
+
+
 
 def fun_1( your_id ):
     ans = hex(your_id)
@@ -7,18 +9,18 @@ def fun_1( your_id ):
 
 def my_integral():
     x = sy.Symbol('x')
-    ans = sy.integrate( sy.exp(-x**2), (x,0, sy.oo))
+    ans = sy.integrate( sy.exp(2.0*x)*sy.sin(x), (x,0,1.0))
     return ans
 
 def my_solution():
-    A = np.array( [[3,1], [1,2]] )
-    b = np.array([9,8])
+    A = np.array( [[3,1,-10], [4,2,-9],[5,3,-8]] )
+    b = np.array([-1,5,11])
     x = np.linalg.solve(A,b) # Solve Ax = b
     return x
 
 
 if __name__ == '__main__':
-    your_id = 12345678
+    your_id = 1300929
     print('Hexadecimal representation of %d is %s'%( your_id, fun_1( your_id) ))
     print('Integral = ', my_integral())
     print('Solution = ', my_solution())
